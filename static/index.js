c65a472304724a5a77dd35613d9bc89093306b25
Matias Menoni <rookiemat@gmail.com>
2022-03-13 17:02:32 -0300

    first commit

---
+++ static/index.js
@@ -0,0 +1,26 @@
+//confirm para que la pagina se ponga modo oscuro 
+let header = document.querySelector('h1')
+let items = document.getElementsByClassName('item')
+let etiqueta = document.querySelector('label')
+let boton = document.querySelector('input')
+
+boton.addEventListener('click', () => {
+    document.body.classList.toggle("dark-body")
+    header.classList.toggle("dark-item")
+    etiqueta.classList.toggle('dark-text')
+    if(header.classList.contains('dark-item')){
+        etiqueta.innerHTML = "Light Mode"
+    }
+    else {
+        etiqueta.innerHTML = "Dark Mode"
+    }
+    for (const item of items) {
+            item.classList.toggle('dark-item')
+            item.classList.toggle('dark-text')
+    }
+})
+
+console.log(header.classList.contains('dark-item'));
+// if(window.confirm("¿Desea que la página se ponga en modo oscuro?")){
+
+// }
